492e987a045c777681fc93871e851a1d08f30a9f
RyanMSmith22 <rymatt22@gmail.com>
2013-10-30 22:27:06 -0500

    Added database creation script

---
+++ CreateDBtables.sql
@@ -0,0 +1,102 @@
+USE [HearthstoneDB]
+GO
+
+/****** Object:  Table [dbo].[GameStats]    Script Date: 10/30/2013 10:25:18 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[GameStats](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[UserHero] [int] NULL,
+	[OpponentHero] [int] NULL,
+	[Outcome] [varchar](10) NULL,
+	[Manaleft] [int] NULL,
+	[Healthleft] [int] NULL,
+	[TS] [date] NULL,
+	[UserID] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[GameStats]  WITH CHECK ADD FOREIGN KEY([OpponentHero])
+REFERENCES [dbo].[Hero] ([ID])
+GO
+
+ALTER TABLE [dbo].[GameStats]  WITH CHECK ADD FOREIGN KEY([UserHero])
+REFERENCES [dbo].[Hero] ([ID])
+GO
+
+ALTER TABLE [dbo].[GameStats]  WITH CHECK ADD FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([ID])
+GO
+
+
+USE [HearthstoneDB]
+GO
+
+/****** Object:  Table [dbo].[Hero]    Script Date: 10/30/2013 10:25:32 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Hero](
+	[ID] [int] NOT NULL,
+	[Name] [varchar](40) NULL,
+	[Class] [varchar](15) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+USE [HearthstoneDB]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 10/30/2013 10:25:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](30) NOT NULL,
+	[Pass] [varchar](20) NULL,
+	[TS] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
